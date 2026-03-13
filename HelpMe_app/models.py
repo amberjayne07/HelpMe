f997732c7aefe828de11e59a3d9de5d08bf6e6e6
Iona Houston <3082512H@student.gla.ac.uk>
2026-03-13 16:41:53 +0000

    Add user manager to match django version

--- HelpMe_app/models.py
+++ HelpMe_app/models.py
@@ -1,16 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.password_validation import validate_password
+import uuid
 
-class User(AbstractBaseUser):
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is required")
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+
+        if password:
+            validate_password(password)
+
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)  # hashes password
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     full_name = models.CharField(max_length=128)
     username = models.CharField(max_length=30, unique=True)
-    password = models.CharField(max_length=16)
     email = models.EmailField(unique=True)
     dateOfBirth = models.DateField()
     picture = models.ImageField(upload_to='user_pics/')
     joinDate = models.DateField(auto_now_add=True)
     passwordHint = models.CharField(max_length=100)
-    
+
     STANDARD = 'STANDARD'
     LIMITED = 'LIMITED'
     ADMIN = 'ADMIN'
@@ -21,10 +46,18 @@ class User(AbstractBaseUser):
     ]
     type = models.CharField(max_length=10, choices=USER_TYPES)
 
+    # REQUIRED for Django admin + authentication
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
 
     def __str__(self):
         return self.username
+    
 
 
 class Category(models.Model):
@@ -57,12 +90,12 @@ class Comment(models.Model):
     postedDate = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.commentID
+        return str(self.commentID)
 
 class Notification(models.Model):
     notificationID = models.UUIDField(primary_key=True)
     questionID = models.ForeignKey(Question, on_delete=models.CASCADE)
-    username = models.CharField(max_length=30)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     
     LIKE = 'LIKE'
     COMMENT = 'COMMENT'
@@ -87,7 +120,7 @@ class Poll(models.Model):
     title = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return self.pollID
+        return self.title
 
 
 class PollItem(models.Model):
@@ -110,4 +143,4 @@ class PollItem(models.Model):
     approvalStatus = models.CharField(max_length=10, choices=APPROVAL_CHOICES, default=NEUTRAL)
 
     def __str__(self):
-        return self.pollItemID
+        return str(self.pollItemID)
